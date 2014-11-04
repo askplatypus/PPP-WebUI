57136c53bf0c69fe1e21b772818fda1f2230ffb7
Tpt <thomaspt@hotmail.fr>
2014-11-04 13:24:55 +0100

    Allows to input question and language with the URL

--- js/main.js
+++ js/main.js
@@ -5,67 +5,76 @@
 (function($, window) {
 	'use strict';
 
-	//Update on install
-
 	var api = new window.pppApi(window.config.pppCoreUrl);
 	var resultBuilder = new window.resultBuilder();
 	var inputParser = new window.inputParser();
 	var $simpleSerarchResult = $('#simplesearch-result');
+	var url = $.url();
+	var languageCode = url.param('lang') ? url.param('lang') : 'en';
 
 
 	function getRandomQuestion() {
 		return 'What is the birth date of George Washington?';
 	}
 
+	function doQuery(question) {
+		$simpleSerarchResult.empty().append(
+			$('<div>')
+				.addClass('panel panel-default')
+				.append(
+				$('<div>')
+					.addClass('panel-body text-center')
+					.append(
+					$('<span>')
+						.addClass('glyphicon glyphicon-refresh glyphicon-refresh-animate')
+				)
+					.append(' Loading...')
+			)
+		);
+
+		var input = inputParser.parse(question);
+		var measures = {
+			'relevance': 0,
+			'accuracy': 1
+		};
+		api.sendRequest(
+			{
+				'language': languageCode,
+				'id': (new Date()).getTime() + '-' + 'webui',
+				'tree': input,
+				'measures': measures,
+				'trace': [
+					{
+						'module': 'input',
+						'tree': input,
+						'measures': measures
+					}
+				]
+			},
+			function(results) {
+				$('#simplesearch-result')
+					.empty()
+					.append(resultBuilder.outputQuery(input))
+					.append(resultBuilder.outputResults(results));
+			}, function(jqXHR, textStatus) {
+				$('#simplesearch-result')
+					.empty()
+					.append(resultBuilder.outputQuery(input))
+					.append(resultBuilder.outputError(textStatus));
+			}
+		);
+	}
+
 	function setupSimpleForm() {
+		var queryQuestion = url.param('q');
+		if(queryQuestion) {
+			$('#simplesearch-input-question').val(queryQuestion);
+			doQuery(queryQuestion);
+		}
+
 		$('#simplesearch-form').submit(function(event) {
 			event.preventDefault();
-
-			$simpleSerarchResult.empty().append(
-				$('<div>')
-					.addClass('panel panel-default')
-					.append(
-						$('<div>')
-							.addClass('panel-body text-center')
-							.append(
-								$('<span>')
-									.addClass('glyphicon glyphicon-refresh glyphicon-refresh-animate')
-							)
-							.append(' Loading...')
-					)
-			);
-
-			var input = inputParser.parse($('#simplesearch-input-question').val());
-			var measures = {
-				'relevance': 0,
-				'accuracy': 1
-			};
-			api.sendRequest(
-				{
-					'language': $('html').attr('lang'),
-					'id': (new Date()).getTime() + '-' + 'webui',
-					'tree': input,
-					'measures': measures,
-					'trace': [
-						{
-							'module': 'input',
-							'tree': input,
-							'measures': measures
-						}
-					]
-				},
-				function(results) {
-					$('#simplesearch-result')
-						.empty()
-						.append(resultBuilder.outputQuery(input))
-						.append(resultBuilder.outputResults(results));
-				}, function(jqXHR, textStatus) {
-					$('#simplesearch-result')
-						.empty()
-						.append(resultBuilder.outputQuery(input))
-						.append(resultBuilder.outputError(textStatus));
-				}
-			);
+			doQuery($('#simplesearch-input-question').val());
 		});
 
 		$('.simplesearch-button-random').click(function() {
