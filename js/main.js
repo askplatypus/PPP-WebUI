2e3f50604c19d4ceee1aca46e1764d1965b0ef1d
Tom Cornebize <tom.cornebize@ens-lyon.fr>
2014-11-09 11:15:54 +0100

    More random questions.

--- js/main.js
+++ js/main.js
@@ -24,8 +24,16 @@
 	}
 
 	function getRandomQuestion() {
-		return 'What is the birth date of George Washington?';
-	}
+		questions=Array(
+			'What is the birth date of George Washington?',
+			'Who is the president of France?',
+			'What is the capital of Australia?',
+			'What is the birth date of the president of the United States?',
+			'Who is the author of "Foundation"?',
+			'Who is the director of "Pulp Fiction"?'
+		);
+		return questions[Math.floor(Math.random()*questions.length)];
+}
 
 	function doQuery(question) {
 		var input = inputParser.parse(question);
