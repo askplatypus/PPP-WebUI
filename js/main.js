cf7063ff6717b988b3977b347d675f37533511bf
Tpt <thomaspt@hotmail.fr>
2014-11-12 21:21:13 +0100

    Fix syntax detail

--- js/main.js
+++ js/main.js
@@ -25,14 +25,14 @@
 	}
 
 	function getRandomQuestion() {
-		var questions = Array(
+		var questions = [
 			'What is the birth date of George Washington?',
 			'Who is the president of France?',
 			'What is the capital of Australia?',
 			'What is the birth date of the president of the United States?',
 			'Who is the author of "Foundation"?',
 			'Who is the director of "Pulp Fiction"?'
-		);
+		];
 		return questions[Math.floor(Math.random()*questions.length)];
     }
 
