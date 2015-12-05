489a0199e870c895e08238543cec2147d8f4d77e
Valentin Lorentz <progval@progval.net>
2015-12-05 11:01:14 +0100

    Use relative protocol (http/https) for fetching context.

--- js/resultBuilder.js
+++ js/resultBuilder.js
@@ -56,7 +56,7 @@
 				var graph = JSON.parse($this.attr('data-jsonld'));
 				var language = $this.attr('lang');
 
-				graph['@context'] = 'http://askplatyp.us/schema-context.jsonld';
+				graph['@context'] = window.location.protocol+''//askplatyp.us/schema-context.jsonld';
 				window.jsonld.expand(graph, function(error, graph) {
 					if(error !== null) {
 						console.log('Invalid JSON-LD: ' + error);
